7c9ebd69e7178de790511edfbc560d0b9721dc78
shadowhunter2155 <gojunyu@gmail.com>
2023-03-27 21:47:02 +0800

    d

--- src/main/java/socket/Client.java
+++ src/main/java/socket/Client.java
@@ -1,14 +1,40 @@
 package socket;
 
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Random;
 
 public class Client {
+	
 	public static void main(String[] args) throws Exception {
-		String host = "localhost";
+		String hostName = "localhost";
 		int port = 5555;
-		Socket socket = new Socket(host,port);
-		System.out.println("success!");
+		
+		// Client 連到 Server 端
+		Socket socket = new Socket(hostName, port);
+		System.out.println("sucess!");
+		
+		// 建立 Client-Server 的 I/O 通訊
+		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		
+		Random random = new Random();
+		while (true) {
+			int number = random.nextInt(10000); 
+			// 傳送給 server 端
+			out.println(number + "");
+			out.flush();
+			//System.out.println(number + "");
+			if(number == 9999) {
+				break;
+			}
+			Thread.sleep((long)(Math.random() * 700 + 200)); // 0.2~0.9 秒發送一次
+		}
+		
 		socket.close();
+		System.out.println("closed");
 	}
+	
 }
--- src/main/java/socket/Server.java
+++ src/main/java/socket/Server.java
@@ -7,24 +7,35 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+	
 	public static void main(String[] args) throws Exception {
+		// 建立 Socket Server
 		ServerSocket serverSocket = new ServerSocket(5555);
-		System.out.println("port:" + serverSocket.getLocalPort());
-		
+		System.out.println("port: " + serverSocket.getLocalPort());
+		// 等待 Client 連入
 		System.out.println("waiting...");
-		Socket clientSocket = serverSocket.accept();
-		System.out.println("success...");
+		Socket clientSocket = serverSocket.accept(); // 接收到 client 連入資訊
+		System.out.println("success!");
 		
-		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+		// 建立 Client-Server 的 I/O 通訊
+		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		
+		// 接收 client 端來的資料
 		String inputLine;
 		while ((inputLine = in.readLine()) != null) {
-			out.print(inputLine);
-			
+			// 將 client 端傳來的資料印出
+			System.out.println("client: " + inputLine);
 		}
 		
+		// 關閉 I/O
+		out.close();
+		in.close();
+		
+		// 關閉連線
 		clientSocket.close();
 		serverSocket.close();
+		System.out.println("closed");
 	}
+	
 }
